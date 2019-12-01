e83d3e1c38b14453062dce4ec96e3cd1f3db259d
agarwalsamarth <52416791+agarwalsamarth@users.noreply.github.com>
2019-12-02 00:02:26 +0530

    Add files via upload

---
+++ arduino_wifi_controlled_bot.ino
@@ -0,0 +1,87 @@
+//for less than avg-white......else black
+//1st ldr is left....2nd for front ldr.....3rd for right ldr
+//0 for black,1 for white
+//3,6 are for left motor....9,11 are for right motor
+////long distance, timeTaken;
+////int trigPin = 2; // trigger pin is for output....we r sending waves from here
+////int echoPin = 3; // echo pin is for input....we r receiving waves here
+
+int r1 = 5, r2 = 6, l1 = 9, l2 = 10;//arduino to l293d
+
+void straight()
+{
+  analogWrite(r1, 204);
+  analogWrite(r2, 0);
+  analogWrite(l1, 202);
+  analogWrite(l2, 0);
+}
+void right()
+{
+  analogWrite(r1, 0);
+  analogWrite(r2, 0);
+  analogWrite(l1, 200);
+  analogWrite(l2, 0);
+}
+void left()
+{
+  analogWrite(r1, 200);
+  analogWrite(r2, 0);
+  analogWrite(l1, 0);
+  analogWrite(l2, 0);
+}
+void back()
+{
+  analogWrite(r1, 0);
+  analogWrite(r2, 204);
+  analogWrite(l1, 0);
+  analogWrite(l2, 202);
+}
+void stopping()
+{
+  analogWrite(r1, 0);
+  analogWrite(r2, 0);
+  analogWrite(l1, 0);
+  analogWrite(l2, 0);
+}
+
+void setup()
+{
+  Serial.begin(9600); // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+
+  pinMode(r1, OUTPUT);
+  pinMode(r2, OUTPUT);
+  pinMode(l1, OUTPUT);
+  pinMode(l2, OUTPUT);
+
+}
+
+void loop()
+{
+  int w = digitalRead(A0);
+  int x = digitalRead(A1);
+  int y = digitalRead(A2);
+  int z = digitalRead(A3);
+  Serial.print(w);
+  Serial.print(x);
+  Serial.print(y);
+  Serial.println(z);
+  
+  if (w == 1 && x == 0 && y == 0 && z == 0)
+    straight();
+
+  else if (w == 0 && x == 1 && y == 0 && z == 0)
+    left();
+
+  else if (w == 0 && x == 0 && y == 1 && z == 0)
+    right();
+
+  else if (w == 0 && x == 0 && y == 0 && z == 1)
+    back();
+
+  else
+    stopping();
+}
---
+++ nodeMCU_wifi_controlled_bot.ino
@@ -0,0 +1,42 @@
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "e1d4667972c44384967897fd5081e1fe"; //You will get this Auth Token in Gmail if you signin Blynk app with your Gmail ID
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Cookie"; //Enter your WIFI Name
+char pass[] = "12345678";//Enter your WIFI Password
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+ 
+  pinMode(D4, OUTPUT);//code 1
+  pinMode(D5, OUTPUT);//code 2
+  pinMode(D6, OUTPUT);//code 3
+  pinMode(D7, OUTPUT);//code 4
+ 
+
+ 
+  digitalWrite(D4, LOW);
+  digitalWrite(D5, LOW);
+  digitalWrite(D6, LOW);
+  digitalWrite(D7, LOW);
+
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+}
+
+void loop()
+{
+  Blynk.run();
+}
